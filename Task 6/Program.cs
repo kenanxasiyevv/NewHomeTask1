06edcfd16c5a07f93d4e74917f32c295ca65df81
kenanxasiyevv <kananakh@code.edu.az>
2022-12-29 14:55:04 +0400

    Add project files.

---
+++ Task 6/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Task_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ay daxil edin: ");
+            int Ay = int.Parse(Console.ReadLine());      
+            int il = Ay / 12;
+            int ay = Ay  % 12 ;
+
+            if (Ay > 1 && Ay < 1188)
+                Console.WriteLine($"{il} il {ay} ay");
+            else
+                Console.WriteLine("Eded verilen araliqda deyil!");
+
+
+
+        }
+    }
+}
